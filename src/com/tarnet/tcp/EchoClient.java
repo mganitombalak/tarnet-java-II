5b96a306ac2a49a08d5fc5b1c3c03981b192dc2e
yuznumaram <aturak@tarimkredi.org.tr>
2020-08-19 17:00:24 +0300

    01

--- src/com/tarnet/tcp/EchoClient.java
+++ src/com/tarnet/tcp/EchoClient.java
@@ -18,6 +18,7 @@ public class EchoClient {
         Future future = client.connect(new InetSocketAddress("localhost", 81));
         future.get();
         String result = sendMsg("deneme", client);
+        System.out.println(result);
     }
 
 
@@ -25,7 +26,7 @@ public class EchoClient {
 
     public static String sendMsg(String message, AsynchronousSocketChannel client) throws ExecutionException, InterruptedException {
         byte[] byteMsg = new String(message).getBytes();
-        ByteBuffer buffer = ByteBuffer.wrap(byteMsg);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
         Future<Integer> writeResult = client.write(buffer);
         writeResult.get();
         buffer.flip();
@@ -33,6 +34,7 @@ public class EchoClient {
         readResult.get();
         String echo = new String(buffer.array()).trim();
         buffer.clear();
+        buffer.get(byteMsg);
         return echo;
     }
 }
