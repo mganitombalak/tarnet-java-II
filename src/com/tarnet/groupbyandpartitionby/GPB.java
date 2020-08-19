ffb99de2a6ed371eac471f8dc8b869fdd2f81b5e
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-08-19 11:34:21 +0300

    GroupBy Usage

--- src/com/tarnet/groupbyandpartitionby/GPB.java
+++ src/com/tarnet/groupbyandpartitionby/GPB.java
@@ -5,22 +5,21 @@ import com.github.javafaker.Faker;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import static java.util.stream.Collectors.*;
 
 public class GPB {
     public static void main(String[] args) {
         List<Employee> employeeList = new LinkedList<>();
-
         IntStream.range(1, 1001).forEach(iterator -> {
             employeeList.add(new Employee(
                     iterator,
                     Faker.instance().name().firstName(),
                     Faker.instance().name().lastName(),
-                    iterator % 5 == 0 ?
+                    iterator % 3 == 0 ?
                             Title.Senior :
-                            iterator % 7 == 0 ? Title.Leader :
-                                    iterator % 11 == 0 ? Title.Manager :
+                            iterator % 11 == 0 ? Title.Leader :
+                                    iterator % 17 == 0 ? Title.Manager :
                                             Title.Employee,
                     Faker.instance().number().randomDouble(2, 5000, 25000),
                     iterator % 5 == 0 ?
@@ -33,7 +32,22 @@ public class GPB {
 
         // GROUP BY DEPARTMENT
 
-        Map<Department, List<Employee>> employeesByDepartment = employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+//        Map<Department, List<Employee>> employeesByDepartment = employeeList.stream().collect(groupingBy(Employee::getDepartment));
+
+        // GROUP BY DEPARTMENT THEN BY TITLE / MULTIPLE FIELD GROUPING
+
+//        Map<Department,Map<Title,List<Employee>>> employeesByDeparmentThenByTitle = employeeList.stream().collect(groupingBy(Employee::getDepartment,groupingBy(Employee::getTitle)));
+
+
+        // GROUP BY DEPARTMENT THEN BY TITLE / AVERAGE SALARY
+
+//        Map<Department,Map<Title,Double>> avaregeSalaryByDepartmentThenByTitle = employeeList
+//                .stream()
+//                .collect(groupingBy(Employee::getDepartment,groupingBy(Employee::getTitle,averagingDouble(Employee::getSalary))));
+
+
+
+
 
 
         System.out.println("OK");
